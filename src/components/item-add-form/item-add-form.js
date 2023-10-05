270ebe746ff1fd7577787b2aa4326351d3372329
a-reznikov <a.reznnikov@gmail.com>
2023-10-05 12:56:56 +0300

    feat: implement form for adding items

--- src/components/item-add-form/item-add-form.js
+++ src/components/item-add-form/item-add-form.js
@@ -4,16 +4,35 @@ import "./item-add-form.css"
 
 export default class ItemAddForm extends Component {
 
+  state = {
+    label: '',
+  }
+
+  onLabelChange = (e) => {
+    this.setState({
+      label: e.target.value,
+    })
+  }
+
+  onSubmit = (e) => {
+    e.preventDefault();
+    console.log(this.state.label);
+    this.props.onItemAdded(this.state.label);
+  }
+
   render() {
-    const { onItemAdded } = this.props;
 
     return (
-      <div className='item-add-form'>
-        <button className='btn btn-primary'
-          onClick={() => onItemAdded('New Task')}>
+      <form className='item-add-form d-flex'
+        onSubmit={this.onSubmit}>
+        <input type='text'
+          className='form-control'
+          placeholder='What task do you want to add?'
+          onChange={this.onLabelChange}></input>
+        <button className='btn btn-primary'>
           Add Item
         </button>
-      </div>
+      </form>
     )
   }
 }
